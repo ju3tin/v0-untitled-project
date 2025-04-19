3b0d1157c1b93738aeebe6ac6ac33bae89c9ab52
ju3tin <ju3tin@hotmail.co.uk>
2025-04-19 16:01:39 +0100

    sdf

--- app/(test)/boxing/page.tsx
+++ app/(test)/boxing/page.tsx
@@ -1,4 +1,5 @@
 // pages/model-viewer.tsx
+"use client"
 import { Canvas } from '@react-three/fiber'
 import { OrbitControls, Environment } from '@react-three/drei'
 import { useEffect, useRef, useState } from 'react'
